fc3ae6d3313239514bd46800929c2e00c59c79e5
Michael Fuget <michaelfuget@gmail.com>
2020-12-03 00:20:47 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
 # Add your code here
+class Dog 
+  attr_accessor :name
+
+  @@all = []
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def self.all 
+    return @@all 
+  end 
+  
+  def self.print_all 
+    @@all.each do |x|
+      puts x.name
+    end 
+  end 
+  
+  def save 
+    @@all.push(self)
+  end
+  
+  def self.clear_all
+    @@all.clear 
+  end 
+  
+end
